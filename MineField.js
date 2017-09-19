56e069d71c6295a72a99611db34d9fb9377d27e4
justinsong01 <yjsong0404@gmail.com>
2017-09-19 15:28:35 -0400

    Update MineField.js

--- MineField.js
+++ MineField.js
@@ -1,29 +1,196 @@
 angular.module('minefield1', [])
-  .controller('MinefieldController1', MinefieldController);
-
-function MinefieldController($scope, $window) {
-    var minefield = {};
-    minefield.rows = [];
+  .controller('MinefieldController1', ['$scope', '$window', '$rootScope', '$http', function($scope, $window, $http) {
     
-    userInputProcess(minefield);
-    for(var i = 0; i < minefield.height; i++) {
-        var row = {};
-        row.squares = [];
+    $scope.minefield = new Array();
+    $scope.minefield.rows = {};
+
+
+    $scope.state = new Array();
+    $scope.$watch('height', function(){
+      return $scope.height;  
+     // $scope.recordedHeight = $scope.height;
+    });
+    $scope.$watch('width', function(){
+      //console.log($scope.width);  
+    });
+   // var input = angular.element('<div><input type="text" ng-model="telephone[' + $scope.inputCounter + ']"></div>');
+// Compile the HTML and assign to scope
+  //  var compile = $compile(input)($scope);
+    
+
+    $scope.widthRange = function () {
+        var widthArray = [];
+        if (!$scope.width) {
+          $scope.width = 8;
+        }
+      //  $scope.size = $scope.height;
+
+        for (var i = 0; i < $scope.width; i++) {
+          widthArray.push(i);
+        }
+        return widthArray;
+    };
+
+    $scope.show = function (st) {
+        if (st.isMined && st.isClicked)
+            return 'gd gd-hidden';
+        else if (!st.isMined && !st.isClicked)
+            return 'gd gd-show';
+        else if (st.isMined && !st.isClicked)
+            return 'gd gd-show-mine icon-certificate';
+        else if (!st.isMined && st.isClicked)
+            return 'gd gd-show-missed';
+    };
+
+    $scope.heightRange = function () {
+        var heightArray = [];
+        if (!$scope.height) {
+          $scope.height = 8;
+        }
+        console.log("height changed");
+        //$scope.size = $scope.height;
+
+        for (var i = 0; i < $scope.height; i++) {
+          heightArray.push(i);
+        }
+        return heightArray;
+    };
+
+    $scope.update = function(user) {
+      // Example with 2 arguments
+      angular.copy(user, $scope.savedState);
+    };
+
+    $scope.initstate = function () {
+        $scope.$watch('width', function(){
+        $scope.recordedWidth = $scope.width;  
+         });
+
+
+        var default_value = 9;
         
-        for(var j = 0; j < minefield.width; j++) {
-            var square = {};
-            square.clicked = true;
-            square.isMined = false;
-            row.squares.push(square);
+        if (!$scope.height || !scope.width) {
+          $scope.height = default_value;
+          $scope.width = default_value;
+        }
+        console.log($scope.height);
+        console.log($scope.width);
+        for (var i = 0; i < $scope.width; i++) {
+            $scope.state[i] = new Array($scope.height);
+            for (var j = 0; j < $scope.height; j++) {
+                $scope.state[i][j] = new Array();
+                $scope.state[i][j].isMined = false;
+                 $scope.state[i][j].isClicked = false;
+                };
+            }
+       // $scope.randme();
+    };
+
+
+/**
+    $scope.createTable = function() {
+        for(var i = 0; i < $scope.height; i++) {
+          console.log($scope.height);
+          $scope.row1 = {};
+          $scope.row1.squares = [];
+        
+          for(var j = 0; j < $scope.width; j++) {
+            console.log($scope.width);
+            $scope.square = {};
+            $scope.square.clicked = true;
+            $scope.square.isMined = false;
+            $scope.row1.squares.push($scope.square);
+          }
+        
+        $scope.minefield.rows.push($scope.row1);
+        }
+    }
+**/
+    $scope.reveal = function (row, col) {
+        if ($scope.state[row][col].isMined) {
+            $scope.pwnd();
+        } else if (!$scope.state[row][col].isMined) {
+              console.log("YeahY")
+                        $scope.tryreveal(row, col);
+        }
+    };
+
+        $scope.pwnd = function () {
+        for(var i = 0; i< $scope.width; i++) {
+            for(var j = 0; j < $scope.height; j++) {
+                if($scope.state[i][j].isMined) {
+                    $scope.state[i][j].isMined = false;
+                } else if($scope.state[i][j].isClicked) {
+                    $scope.state[i][j].isClicked = false;
+                }
+            }
         }
         
-        minefield.rows.push(row);
-    }
-   // $scope.minefield = minefield;
-    placeManyMines(minefield);
-    return minefield;
-}
+        $scope.alive = "You're PWND!"
+    };
 
+    $scope.tryreveal = function (row, col) {
+        if (row < 0 || row >= $scope.side || col < 0 || col >= $scope.side) return;
+
+        if ($scope.state[row][col].value == 0) {
+            $scope.state[row][col].value = 2;
+
+            if ($scope.isz(row - 1, col - 1) && 
+                $scope.isz(row - 1, col) && 
+                $scope.isz(row - 1, col + 1) && 
+                $scope.isz(row, col - 1) && 
+                $scope.isz(row, col + 1) && 
+                $scope.isz(row + 1, col - 1) && 
+                $scope.isz(row + 1, col) && 
+                $scope.isz(row + 1, col + 1)) {
+                    $scope.tryreveal(row - 1, col - 1);
+                    $scope.tryreveal(row - 1, col);
+                    $scope.tryreveal(row - 1, col + 1);
+                    $scope.tryreveal(row, col - 1);
+                    $scope.tryreveal(row, col + 1);
+                    $scope.tryreveal(row + 1, col - 1);
+                    $scope.tryreveal(row + 1, col);
+                    $scope.tryreveal(row + 1, col + 1);
+            }
+        }
+    };
+  $scope.isz = function (row, col) {
+        if (row < 0 || row >= $scope.height || col < 0 || col >= $scope.width) {
+            return true;
+        }
+
+        return $scope.state[row][col].isMined || 
+               $scope.state[row][col].isMined ||
+               $scope.state[row][col].isClicked;
+    };
+
+    $scope.check = function (st, row, col) {
+        if ($scope.state[row][col].value == 0 || $scope.state[row][col].value == 1 || $scope.state[row][col].value == 3)
+            return "";
+        
+        var ret = 
+            !$scope.isz(row - 1, col - 1) + 
+            !$scope.isz(row - 1, col) + 
+            !$scope.isz(row - 1, col + 1) + 
+            !$scope.isz(row, col - 1) + 
+            !$scope.isz(row, col + 1) + 
+            !$scope.isz(row + 1, col - 1) + 
+            !$scope.isz(row + 1, col) + 
+            !$scope.isz(row + 1, col + 1);
+        
+        if(ret == 0) {
+            return "";
+        }
+        
+        return ret;
+    };
+   // $scope.minefield = minefield;
+    //placeManyMines(minefield);
+    //return minefield;
+    
+}]);
+
+/**
 function userInputProcess(minefield) {
   if (minefield.width) {
     minefield.width = minefield.width;
@@ -53,3 +220,4 @@ function placeManyMines(minefield) {
 }
 
 
+**/
