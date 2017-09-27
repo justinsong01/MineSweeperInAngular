01d1b62ec6a1721df8c5a515af7f26de4f7afe3a
justinsong01 <yjsong0404@gmail.com>
2017-09-27 14:29:32 -0400

    Update MineField.js
    added functionality of being live whenever the code source (height, width) change.

--- MineField.js
+++ MineField.js
@@ -3,7 +3,12 @@ angular.module('minefield1', [])
     
     $scope.minefield = new Array();
     $scope.minefield.rows = {};
-
+    $scope.modes = ["Easy", "Normal", "Difficult", "Custom"];
+    $scope.mode = "Easy";
+    $scope.gameDisable = false;
+    $scope.result = "";
+    $scope.customDisable = true;
+    
 
     $scope.state = new Array();
     $scope.$watch('height', function(){
@@ -13,15 +18,12 @@ angular.module('minefield1', [])
     $scope.$watch('width', function(){
       //console.log($scope.width);  
     });
-   // var input = angular.element('<div><input type="text" ng-model="telephone[' + $scope.inputCounter + ']"></div>');
-// Compile the HTML and assign to scope
-  //  var compile = $compile(input)($scope);
     
 
     $scope.widthRange = function () {
         var widthArray = [];
         if (!$scope.width) {
-          $scope.width = 8;
+          $scope.width = 9;
         }
       //  $scope.size = $scope.height;
 
@@ -30,6 +32,42 @@ angular.module('minefield1', [])
         }
         return widthArray;
     };
+
+
+
+    $scope.heightRange = function () {
+        var heightArray = [];
+        if (!$scope.height) {
+          $scope.height = 9;
+        }
+
+        for (var i = 0; i < $scope.height; i++) {
+          heightArray.push(i);
+        }
+        return heightArray;
+    };
+
+    $scope.updateMode = function() {
+      if ($scope.mode == "Easy") {
+        $scope.width = 9;
+        $scope.height = 9;
+        $scope.mine = 9;
+        $scope.customDisable = true;
+      } else if ($scope.mode == "Normal") {
+        $scope.width = 16;
+        $scope.height = 16;
+        $scope.mine = 12;
+      $scope.customDisable = true;
+      } else if ($scope.mode == "Difficult") {
+        $scope.width = 20;
+        $scope.height = 20;
+        $scope.mine = 18;
+        $scope.customDisable = true;
+      } else if ($scope.mode == "Custom") {
+        $scope.customDisable = false;
+         return 0;
+      }
+    }
 
     $scope.show = function (st) {
         if (st.isMined && st.isClicked)
@@ -42,76 +80,59 @@ angular.module('minefield1', [])
             return 'gd gd-show-missed';
     };
 
-    $scope.heightRange = function () {
-        var heightArray = [];
-        if (!$scope.height) {
-          $scope.height = 8;
-        }
-        console.log("height changed");
-        //$scope.size = $scope.height;
-
-        for (var i = 0; i < $scope.height; i++) {
-          heightArray.push(i);
-        }
-        return heightArray;
-    };
-
     $scope.update = function(user) {
       // Example with 2 arguments
       angular.copy(user, $scope.savedState);
     };
+
+    $scope.gameInspect = function() {
+      $scope.game = !$scope.game;
+      console.log($scope.game);
+      $scope.initstate();
+      console.log($scope.customEnable);
+
+    } 
 
     $scope.initstate = function () {
         $scope.$watch('width', function(){
         $scope.recordedWidth = $scope.width;  
          });
 
-
+        
         var default_value = 9;
         
-        if (!$scope.height || !scope.width) {
-          $scope.height = default_value;
-          $scope.width = default_value;
-        }
-        console.log($scope.height);
-        console.log($scope.width);
-        for (var i = 0; i < $scope.width; i++) {
+          if (!$scope.height || !scope.width) {
+            $scope.height = default_value;
+            $scope.width = default_value;
+            $scope.mine = default_value;
+          }
+        
+          console.log($scope.height);
+          console.log($scope.width);
+          for (var i = 0; i < $scope.width; i++) {
             $scope.state[i] = new Array($scope.height);
             for (var j = 0; j < $scope.height; j++) {
                 $scope.state[i][j] = new Array();
                 $scope.state[i][j].isMined = false;
-                 $scope.state[i][j].isClicked = false;
+                $scope.state[i][j].isClicked = false;
+               
                 };
             }
        // $scope.randme();
+      
     };
+    //  0 - hidden empty
+    //  1 - hidden mine
+    //  2 - revealed empty
+    //  3 - revealed mine
+    //  4 - missed empty
 
-
-/**
-    $scope.createTable = function() {
-        for(var i = 0; i < $scope.height; i++) {
-          console.log($scope.height);
-          $scope.row1 = {};
-          $scope.row1.squares = [];
-        
-          for(var j = 0; j < $scope.width; j++) {
-            console.log($scope.width);
-            $scope.square = {};
-            $scope.square.clicked = true;
-            $scope.square.isMined = false;
-            $scope.row1.squares.push($scope.square);
-          }
-        
-        $scope.minefield.rows.push($scope.row1);
-        }
-    }
-**/
     $scope.reveal = function (row, col) {
         if ($scope.state[row][col].isMined) {
             $scope.pwnd();
         } else if (!$scope.state[row][col].isMined) {
-              console.log("YeahY")
-                        $scope.tryreveal(row, col);
+            console.log("YeahY")
+            $scope.tryreveal(row, col);
         }
     };
 
@@ -119,14 +140,14 @@ angular.module('minefield1', [])
         for(var i = 0; i< $scope.width; i++) {
             for(var j = 0; j < $scope.height; j++) {
                 if($scope.state[i][j].isMined) {
-                    $scope.state[i][j].isMined = false;
-                } else if($scope.state[i][j].isClicked) {
-                    $scope.state[i][j].isClicked = false;
+                    $scope.state[i][j].isClicked = true;
+                } else if(!$scope.state[i][j].isMined) {
+                    $scope.state[i][j].isClicked = true;
                 }
             }
         }
         
-        $scope.alive = "You're PWND!"
+        $scope.result = "You're PWND!"
     };
 
     $scope.tryreveal = function (row, col) {
@@ -165,9 +186,9 @@ angular.module('minefield1', [])
     };
 
     $scope.check = function (st, row, col) {
-        if ($scope.state[row][col].value == 0 || $scope.state[row][col].value == 1 || $scope.state[row][col].value == 3)
+        if ($scope.state[row][col].value == 0 || $scope.state[row][col].value == 1 || $scope.state[row][col].value == 3) {
             return "";
-        
+        }
         var ret = 
             !$scope.isz(row - 1, col - 1) + 
             !$scope.isz(row - 1, col) + 
